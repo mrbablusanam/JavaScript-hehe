e20ae5c7b4843dc99ec436c13141f996d8e139e7
MrSanam <mdbadruddinhassan@gmail.com>
2025-01-06 11:21:50 +0530

    Intro-Variable-Datatype

---
+++ level 1/PracticeSet-1.js
@@ -0,0 +1,10 @@
+//Question 1: Create a variable of type string and try to add a number to it.
+var str = "Hello";
+var num = 25;
+console.log(str+num);
+
+//Quetion 2: Use typeof operator and find the datatype of the variable in last question.
+console.log(typeof(str));
+console.log(typeof(num));
+
+//Quetion 3: Create a const object in javascript. Can you change it to hold a number later? 
